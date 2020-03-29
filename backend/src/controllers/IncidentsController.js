726c96a1177f1579b6d3188f8efd2551b54ef818
Lucio Fonseca <projeto02@penseavanti.com.br>
2020-03-29 19:48:11 -0300

    fix: alterações e adição de funcionalidades

--- backend/src/controllers/IncidentsController.js
+++ backend/src/controllers/IncidentsController.js
@@ -14,7 +14,7 @@ module.exports = {
         response.header('X-Total-Count', count['count(*)']);
         return response.json(incidents);
     },
-    async create(request, response) {
+    async create(request, response) { 
         const {title, description, value} = request.body;
         const ong_id = request.headers.authorization;
 
